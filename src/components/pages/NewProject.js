663668cbb70d16189ef4d494ad4b6d9cdd098719
thales0591 <thales0591@gmail.com>
2024-06-05 10:42:57 -0300

    create project form components

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -6,7 +6,7 @@ function NewProject() {
         <div className={styles.newproject_container}>
             <h1>Crie seu projeto</h1>
             <p>Crie seu projeto para depois adicionar os serviços</p>
-            <ProjectForm/>
+            <ProjectForm btnText="Criar Projeto"/>
         </div>
     )
 }
