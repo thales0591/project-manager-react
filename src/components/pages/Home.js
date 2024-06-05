a0a364040ba95569e73fc37c89285c087e581808
thales0591 <thales0591@gmail.com>
2024-06-05 09:14:35 -0300

    create Project folder and js of ProjectForm

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,10 @@
-import style from './Home.module.css'
+import styles from './Home.module.css'
 import savings from '../../img/savings.svg'
 import LinkButton from '../layout/LinkButton'
 
 function Home() {
     return (
-        <section className={style.home_container}>
+        <section className={styles.home_container}>
             <h1>Bem-vindo ao <span>Costs</span></h1>
             <p>Comece a gerenciar os seus projetos agora mesmo!</p>
             <LinkButton to="/newproject" text="Criar Projeto"/>
--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,6 +1,13 @@
+import ProjectForm from '../project/ProjectForm'
+import styles from './NewProject.module.css'
+
 function NewProject() {
     return (
-        <h1>Novo Projeto</h1>
+        <div className={styles.newproject_container}>
+            <h1>Crie seu projeto</h1>
+            <p>Crie seu projeto para depois adicionar os serviços</p>
+            <ProjectForm/>
+        </div>
     )
 }
 
