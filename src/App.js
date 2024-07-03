0c6183f1c2144386d4544d5fb5f14de4afb8376f
thales0591 <thales0591@gmail.com>
2024-07-03 19:51:28 -0300

    create Project.js edition page and GET fetch to bring projects data

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Container from './components/layout/Container'
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
 import Projects from './components/pages/Projects'
+import Project from './components/pages/Project'
 
 function App() {
   return (
@@ -15,10 +16,11 @@ function App() {
       <Container customClass="min-height">
         <Routes>
           <Route exact path="/" element={<Home/>}/>
-          <Route exact path="/company" element={<Company/>}/>
-          <Route exact path="/contact" element={<Contact/>}/>
-          <Route exact path="/newproject" element={<NewProject/>}/>
-          <Route exact path="/projects" element={<Projects/>}/>
+          <Route path="/company" element={<Company/>}/>
+          <Route path="/contact" element={<Contact/>}/>
+          <Route path="/newproject" element={<NewProject/>}/>
+          <Route path="/projects" element={<Projects/>}/>
+          <Route path="/project/:id" element={<Project/>}/>
         </Routes>
       </Container>
       <Footer />
