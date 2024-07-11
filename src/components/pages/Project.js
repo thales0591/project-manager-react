adde98e6c71e1e3b7168417c388ac5dd4f7c5a87
thales0591 <thales0591@gmail.com>
2024-07-11 18:28:58 -0300

    create services area

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -11,6 +11,7 @@ function Project() {
     const {id} = useParams()
     const [project, setProject] = useState([])
     const [showProjectForm, setShowProjectForm] = useState(false)
+    const [showServiceForm, setServiceForm] = useState(false)
     const [message, setMessage] = useState()
     const [type, setType] = useState()
 
@@ -32,6 +33,9 @@ function Project() {
     }, [id])
 
     function editPost(project) {
+
+        setMessage('')
+
         if(project.budget < project.cost) {
             setMessage('O orçamento não pode ser menor que o custo do projeto!')
             setType('error')
@@ -56,6 +60,10 @@ function Project() {
 
     function toggleProjectForm() {
             setShowProjectForm(!showProjectForm)
+    }
+
+    function toggleServiceForm() {
+        setServiceForm(!showServiceForm)
     }
 
     return (
@@ -92,6 +100,19 @@ function Project() {
                         </div>
                     )}
                 </div>
+                <div className={styles.service_form_container}>
+                    <h2>Adicione um serviço:</h2>
+                    <button className={styles.btn}  onClick={toggleServiceForm}>
+                        {!showServiceForm ? 'Adicionar serviço' : 'Fechar'}
+                    </button>
+                    <div className={styles.project_info}>
+                    {showServiceForm && <div>Formulário do serviço</div>}
+                    </div>
+                </div>
+                <h2>Serviços</h2>
+                <Container customClass="start">
+                    <p>itens de Serviços</p>
+                </Container>
                 </Container>
             </div>
             
