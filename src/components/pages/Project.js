e8f4123849b476b1a002df08dc455b7ba44d817f
thales0591 <thales0591@gmail.com>
2024-07-16 21:42:30 -0300

    create delete service function and PATCH fetch

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -95,7 +95,33 @@ function Project() {
 
     }
 
-    function removeService() {
+    function removeService(id, cost) {
+
+        const servicesUpdated = project.services.filter(
+            (service) => service.id !== id
+        )
+
+        const projectUpdated = project
+
+        projectUpdated.services = servicesUpdated
+        projectUpdated.cost = parseFloat(projectUpdated.cost) - parseFloat(cost)
+
+        fetch(`http://localhost:5000/projects/${projectUpdated.id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(projectUpdated)
+        })
+        .then(resp => resp.json())
+        .then((data) => {
+            setProject(projectUpdated)
+            setServices(servicesUpdated)
+            setMessage('Serviço removido com sucesso!')
+            setType('success')
+            setTimeout(() => {setMessage('');}, 3000)
+        })
+        .catch(err => console.log(err))
 
     }
 
